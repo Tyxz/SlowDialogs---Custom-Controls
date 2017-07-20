0468dbabd1bfd09596fc3c99bd55435ec3c3d75c
Arne Rantzen <arne.rantzen@mailbox.org>
2017-07-20 13:44:17 +0200

    Version 1.0.0
    Adds the option to skip by mouse click or by a custom keybinding.

---
+++ lang/en.lua
@@ -0,0 +1,7 @@
+SDCC.loc = {
+	SDCC_SKIP_TITLE = "Skip the Slow Dialog animation",
+	ST_ANY_NAME 	= "Skip with mouse",
+	ST_ANY_DESC 	= "Skip the animation when pressing the mouse button."
+}
+
+ZO_CreateStringId("SI_BINDING_NAME_SDCC_SKIP_TITLE", SDCC.loc.SDCC_SKIP_TITLE)
