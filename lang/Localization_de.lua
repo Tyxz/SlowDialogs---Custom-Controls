85952be25d768f20a56be8031243a07b52a34fd5
Arne Rantzen <arne.rantzen@mailbox.org>
2017-07-20 20:38:07 +0200

    Add german translationn for SlowDialogs

---
+++ lang/Localization_de.lua
@@ -0,0 +1,13 @@
+local SlowDialogs = SlowDialogsGlobal
+
+SlowDialogs.displayName = "Langsame Dialoge"
+SlowDialogs.author = "Shinni"
+
+SlowDialogs.delayOption = "Verzögerung"
+SlowDialogs.delayTooltip = "Verzögerung zwischen zwei Zeichen in Millisekunden."
+
+SlowDialogs.startDelayOption = "Anfängliche Verzögerung"
+SlowDialogs.startDelayTooltip = "Verzögerung bis der Text anfängt zu erscheinen."
+
+SlowDialogs.fadeLengthOption = "Einblendlänge"
+SlowDialogs.fadeLengthTooltip = "Länger der Einblendanimation."
